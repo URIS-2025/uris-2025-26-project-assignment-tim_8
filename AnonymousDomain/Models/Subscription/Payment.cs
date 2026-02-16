bfd2c2839e543c1eef226fd3c5aa90a522a3d622
Oliv3r300 <conkaoliver@gmail.com>
2026-02-16 20:57:29 +0100

    Zavrseni modeli za Subsription, Payment i SubscriptionPlan

---
+++ AnonymousDomain/Models/Subscription/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousDomain.Models.Subscription
+    {
+    internal class Payment
+    {
+        public Guid Id { get; set; }
+
+        public double Total {  get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+
+    }
+}
