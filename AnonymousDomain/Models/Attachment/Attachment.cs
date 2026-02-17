bea34e7698d9cf646434b0b5f94d8ffacc3b884e
Uros Zec <zecuros23@gmail.com>
2026-02-17 12:14:46 +0100

    SuggestionBox i Attachment models

---
+++ AnonymousDomain/Models/Attachment/Attachment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousDomain.Models.Attachment
+{
+    public class Attachment
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+
+        public string Url { get; set; }
+
+        public DateTime UploadedAt { get; set; }
+
+        public Guid ProblemId { get; set; }
+        public Guid SuggestionID { get; set; }
+
+
+    }
+}
