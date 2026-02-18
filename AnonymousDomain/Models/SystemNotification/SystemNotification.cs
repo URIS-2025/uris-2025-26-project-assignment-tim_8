137ebc27034668b6cabc605ee0089390fb5f9506
Njami03 <moskalj.minja@gmail.com>
2026-02-18 18:03:38 +0100

    enum Type

--- AnonymousDomain/Models/SystemNotification/SystemNotification.cs
+++ AnonymousDomain/Models/SystemNotification/SystemNotification.cs
@@ -5,21 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AnonymousDomain.Models.BillingNotification
+namespace AnonymousDomain.Models.SystemNotification
 {
-    public class BillingNotification
+    public class SystemNotification
     {
         public Guid Id { get; set; }
 
         public string Text { get; set; }
 
-        public bool IsRead {  get; set; }
+        public bool IsRead { get; set; }
+
+        public DateTime CreatedAt { get; set; }
 
         public Guid OrganizationId { get; set; }
 
-        public Guid PaymentId { get; set; }
+        public Guid AnonymousUserId { get; set; }
+
+        public Guid ProblemCommentId { get; set; }
+
+        public Guid SuggestionCommentId { get; set; }
 
         public TypeStatus Type {  get; set; }
-        
+
     }
 }
