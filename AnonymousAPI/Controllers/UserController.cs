87df5fc6bec47fec4e1069fc5ddff7d4881107a4
Njami03 <moskalj.minja@gmail.com>
2026-02-20 15:45:46 +0100

    Controlleri za User, UserRole,AnonymousUser

---
+++ AnonymousAPI/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using AnonymousDomain.Models.Organization;
+using Azure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnonymousAPI.Controllers
+{
+    //[Authorize] 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+
+        //private readonly IUserService _userService;
+        //private readonly IMapper _mapper;
+        public UserController(/* IUserService userService, IMapper mapper*/)
+        {
+            // _userService = userService;
+            // _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAllUsers() //TODO Promeniti povratnu vrednost na DTO,
+        {
+            // var result = _userService.GetAll();
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUserById(Guid id) //TODO Promeniti povratnu vrednost na DTO
+        {
+            // var result = _userService.GetById(id);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpPost]
+        public ActionResult<User> CreateUser([FromBody] User user ) //TODO Promeniti povratnu vrednost na DTO, promeniti prosledjenu vrednost na DTO
+        {
+            // var result = _userService.Create(user);
+            // return Created("", result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpPut]
+        public ActionResult<User> UpdateUser([FromBody] User user ) //TODO Promeniti povratnu vrednost na DTO, promeniti prosledjenu vrednost na DTO
+        {
+            // var result = _userService.Update(user);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(Guid id)
+        {
+            // _userService.Delete(id);
+            // return NoContent();
+            return null; // TODO skloniti kada se urade servisi 
+        }
+    }
+}
---
+++ AnonymousAPI/Controllers/UserRoleController.cs
@@ -0,0 +1,62 @@
+﻿using AnonymousDomain.Models.Organization;
+using Azure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnonymousAPI.Controllers
+{
+    //[Authorize] 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserRoleController : Controller
+    {
+
+        //private readonly IUserRoleService _userRoleService;
+        //private readonly IMapper _mapper;
+        public UserRoleController(/* IUserRoleService userRoleService, IMapper mapper*/)
+        {
+            // _userRoleService = userRoleService;
+            // _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UserRole>> GetAllUserRoles() //TODO Promeniti povratnu vrednost na DTO,
+        {
+            // var result = _userRoleService.GetAll();
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<UserRole> GetUserRoleById(Guid id) //TODO Promeniti povratnu vrednost na DTO
+        {
+            // var result = _userRoleService.GetById(id);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpPost]
+        public ActionResult<UserRole> CreateUserRole([FromBody] UserRole userRole) //TODO Promeniti povratnu vrednost na DTO, promeniti prosledjenu vrednost na DTO
+        {
+            // var result = _userRoleService.Create(userRole);
+            // return Created("", result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpPut]
+        public ActionResult<UserRole> UpdateUserRole([FromBody] UserRole userRole) //TODO Promeniti povratnu vrednost na DTO, promeniti prosledjenu vrednost na DTO
+        {
+            // var result = _userRoleService.Update(userRole);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUserRole(Guid id)
+        {
+            // _userRoleService.Delete(id);
+            // return NoContent();
+            return null; // TODO skloniti kada se urade servisi 
+        }
+    }
+}
