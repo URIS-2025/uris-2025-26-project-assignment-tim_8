34f0f246c9705464385edc470d82a2ef7b1ba00b
VOJABENGERLT\vojin <vojingalic03@gmail.com>
2026-02-20 15:39:33 +0100

    All controllers done for suggestions

---
+++ AnonymousAPI/Controllers/SuggestionCommentController.cs
@@ -0,0 +1,67 @@
+﻿using AnonymousDomain.Models.Suggestion;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnonymousAPI.Controllers
+{
+    //[Authorize] 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuggestionCommentController : Controller
+    {
+        //private readonly ISuggestionCommentService _suggestionCommentService;
+        //private readonly IMapper _mapper;
+        public SuggestionCommentController(/* ISuggestionCommentService suggestionCommentService, IMapper mapper*/)
+        {
+            // _suggestionCommentService = suggestionCommentService;
+            // _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<SuggestionComment>> GetAllSuggestionComments() //TODO Promeniti povratnu vrednost na DTO,
+        {
+            // var result = _suggestionCommentService.GetAll();
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<SuggestionComment> GetSuggestionCommentById(Guid id) //TODO Promeniti povratnu vrednost na DTO
+        {
+            // var result = _suggestionCommentService.GetById(id);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpGet("suggestion/{id}")]
+        public ActionResult<IEnumerable<Suggestion>> GetSuggestionCommentsBySuggestionId(Guid id) //TODO Promeniti povratnu vrednost na DTO
+        {
+            // var result = _suggestionCommentService.GetBySuggestionId(id);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpPost]
+        public ActionResult<SuggestionComment> CreateSuggestionComment([FromBody] SuggestionComment suggestionComment) //TODO Promeniti povratnu vrednost na DTO, promeniti prosledjenu vrednost na DTO
+        {
+            // var result = _suggestionCommentService.Create(suggestionComment);
+            // return Created("", result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpPut]
+        public ActionResult<SuggestionComment> UpdateSuggestionComment([FromBody] SuggestionComment suggestionComment) //TODO Promeniti povratnu vrednost na DTO, promeniti prosledjenu vrednost na DTO
+        {
+            // var result = _suggestionCommentService.Update(suggestionComment);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSuggestionComment(Guid id)
+        {
+            // _suggestionCommentService.Delete(id);
+            // return NoContent();
+            return null; // TODO skloniti kada se urade servisi 
+        }
+    }
+}
---
+++ AnonymousAPI/Controllers/SuggestionController.cs
@@ -0,0 +1,67 @@
+﻿using AnonymousDomain.Models.Suggestion;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnonymousAPI.Controllers
+{
+    //[Authorize] 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuggestionController : Controller
+    {
+        //private readonly ISuggestionService _suggestionService;
+        //private readonly IMapper _mapper;
+        public SuggestionController(/* ISuggestionService suggestionService, IMapper mapper*/)
+        {
+            // _suggestionService = suggestionService;
+            // _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Suggestion>> GetAllSuggestions() //TODO Promeniti povratnu vrednost na DTO,
+        {
+            // var result = _suggestionService.GetAll();
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Suggestion> GetSuggestionById(Guid id) //TODO Promeniti povratnu vrednost na DTO
+        {
+            // var result = _suggestionService.GetById(id);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpGet("user/{id}")]
+        public ActionResult<IEnumerable<Suggestion>> GetSuggestionsByUserId(Guid id) //TODO Promeniti povratnu vrednost na DTO
+        {
+            // var result = _suggestionService.GetByUserId(id);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpPost]
+        public ActionResult<Suggestion> CreateSuggestion([FromBody] Suggestion suggestion) //TODO Promeniti povratnu vrednost na DTO, promeniti prosledjenu vrednost na DTO
+        {
+            // var result = _suggestionService.Create(suggestion);
+            // return Created("", result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpPut]
+        public ActionResult<Suggestion> UpdateSuggestion([FromBody] Suggestion suggestion) //TODO Promeniti povratnu vrednost na DTO, promeniti prosledjenu vrednost na DTO
+        {
+            // var result = _suggestionService.Update(suggestion);
+            // return Ok(result);
+            return null; // TODO skloniti kada se urade servisi
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSuggestion(Guid id)
+        {
+            // _suggestionService.Delete(id);
+            // return NoContent();
+            return null; // TODO skloniti kada se urade servisi 
+        }
+    }
+}
