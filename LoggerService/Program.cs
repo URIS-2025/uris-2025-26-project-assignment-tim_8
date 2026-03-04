22dba312e8b4cb922d9e2de742c6453f35ffd6e9
VOJABENGERLT\vojin <vojingalic03@gmail.com>
2026-03-04 15:45:38 +0100

    fix

--- LoggerService/Program.cs
+++ LoggerService/Program.cs
@@ -21,6 +21,12 @@ builder.Services.AddDbContext<LoggerContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<LoggerContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
